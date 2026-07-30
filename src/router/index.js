012af68313db315f7b08b9fecfc9e1b2d0dd2009
TTS <theintheinsoe1218@gmail.com>
2026-07-30 11:59:15 +0630

    new sale

--- src/router/index.js
+++ src/router/index.js
@@ -158,6 +158,7 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/admin/SaleList.vue'),
     },
+    
      
   ],
 })
--- src/views/admin/Sale.vue
+++ src/views/admin/Sale.vue
@@ -1,224 +1,387 @@
 <template>
-  <v-container fluid>
-    <!-- Header -->
+  <v-container fluid class="pa-0 ma-0">
+    <v-row>
+      <!-- Left -->
+      <v-col cols="12" md="8">
+        <v-card elevation="0">
+          <v-table fixed-header height="460px" density="compact" class="c-table">
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>Product Name</th>
+                <th>Qty</th>
+                <th>Unit Price</th>
+                <th>Amount</th>
+                <th>Discount</th>
+                <th>Balance</th>
+                <th>Action</th>
+              </tr>
+            </thead>
 
-    <div class="d-flex justify-end mb-4">
-      <v-btn class="add-btn" prepend-icon="mdi-plus" @click="dialog = true">
-        Add UserAccount
-      </v-btn>
-    </div>
+            <tbody>
+              <tr
+                v-for="(item, index) in itemList"
+                :key="index"
+                @click="selectedIndex = index"
+                :style="{
+                  backgroundColor: selectedIndex === index ? '#f5e2e5' : 'transparent',
+                }"
+              >
+                <td>{{ index + 1 }}</td>
 
-    <!-- Table Card -->
-    <v-card rounded="lg" elevation="0">
-      <v-table fixed-header height="400px" density="compact" class="c-table">
-        <thead>
-          <tr>
-            <th class="text-center">No.</th>
-            <th class="text-center">Township Name</th>
-            <th class="text-center" width="150">Action</th>
-          </tr>
-        </thead>
+                <td class="text-start">{{ item.itemtransactionDto?.productDto?.title }}</td>
+                <td class="text-end">{{ item.itemtransactionDto.qty }}</td>
+                <td class="text-end">{{ item.itemtransactionDto.unitPrice }}</td>
+                <td class="text-end">{{ item.itemtransactionDto.amount }}</td>
+                <td class="text-end">{{ item.itemtransactionDto.discount }}</td>
+                <td class="text-end">{{ item.itemtransactionDto.balance }}</td>
+                <td class="text-center">
+                  <v-btn icon size="small" color="primary" @click.stop="editItem(item)">
+                    <v-icon>mdi-pencil</v-icon>
+                  </v-btn>
 
-        <tbody>
-          <tr
-            v-for="(item, index) in townshipList"
-            :key="item.townshipId"
-            @click="selectedOne = item"
-            :style="{
-              backgroundColor:
-                item.townshipId == selectedOne.townshipId ? '#f5e2e5' : 'transparent',
-            }"
-          >
-            <td class="text-center">{{ index + 1 }}</td>
-
-            <td class="text-center">{{ item.townshipName }}</td>
-
-            <td class="text-center">
-              <v-btn density="compact" icon="mdi-pencil" @click="editCity(item)"></v-btn>
-              <v-btn density="compact" icon="mdi-delete" @click="deletCity(item)"></v-btn>
-            </td>
-          </tr>
-        </tbody>
-      </v-table>
-    </v-card>
-
-    <!-- Add Dialog -->
-    <v-dialog v-model="dialog" max-width="500" persistent>
-      <v-card rounded="xl" class="cdialog">
-        <!-- Header -->
-        <div class="dialog-header">
-          <div class="d-flex align-center">
-            <div>
-              <div class="text-h6 font-weight-bold">Add New City</div>
-            </div>
-          </div>
-        </div>
-
-        <v-card-text class="pa-6">
-          <v-text-field
-            v-model="townshipDto.townshipName"
-            class="cinput"
-            label="Township Name"
-            variant="outlined"
-            density="compact"
-          />
-        </v-card-text>
-
-        <v-divider />
-
-        <v-card-actions class="pa-4">
-          <v-spacer />
-
-          <v-btn variant="tonal" rounded="pill" class="mr-2" @click="dialog = false">
-            Cancel
-          </v-btn>
-
-          <v-btn rounded="pill" class="add-btn" @click="saveCity"> {{ saveOrUpdate }}</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-
-    <!-- Delete Dialog  -->
-    <v-col>
-      <v-dialog v-model="dialogDelete" width="500">
-        <v-card>
-          <v-card-title class="text-h5 white--text bg-red"> Delete </v-card-title>
-
-          <v-card-text class="text-h6">
-            Are you sure to delete({{ selectedOne.name }})?
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn class="black" text @click="dialogDelete = false"> CANCEL </v-btn>
-            <v-btn dark class="bg-red" text @click="clickDeleteDialog()"> DELETE </v-btn>
-          </v-card-actions>
+                  <v-btn icon size="small" color="red" @click.stop="deleteItem(item)">
+                    <v-icon>mdi-delete</v-icon>
+                  </v-btn>
+                </td>
+              </tr>
+            </tbody>
+          </v-table>
         </v-card>
-      </v-dialog>
-    </v-col>
+      </v-col>
+
+      <!-- Right -->
+      <v-col cols="12" md="4">
+        <v-card rounded="xl" elevation="0">
+          <v-card-text>
+            <v-row no-gutters>
+              <v-col cols="12" md="12">
+                <v-menu
+                  v-model="fromMenu"
+                  full-width
+                  max-width="290px"
+                  min-width="290px"
+                  v-bind:close-on-content-click="false"
+                >
+                  <template v-slot:activator="{ props }">
+                    <v-text-field
+                      class="cinput"
+                      v-model="saleDto.receivedDate"
+                      label="Received Date"
+                      readonly
+                      v-bind="props"
+                      density="compact"
+                      variant="outlined"
+                    >
+                    </v-text-field>
+                  </template>
+                  <v-date-picker
+                    v-model="receivedPicker"
+                    color="primary"
+                    hide-header
+                    :max="new Date()"
+                  ></v-date-picker>
+                </v-menu>
+              </v-col>
+
+              <v-col cols="12" md="12">
+                <v-autocomplete
+                  v-model="saleDto.customerDto"
+                  item-text="userName"
+                  item-title="userName"
+                  :items="customerList"
+                  label="Customer"
+                  name="userName"
+                  return-object
+                  required
+                  density="compact"
+                  variant="outlined"
+                  filled
+                  class="cinput"
+                />
+              </v-col>
+              <div style="border: 1px solid grey" class="pa-2">
+                <v-row no-gutters>
+                  <v-col cols="12" md="12">
+                    <v-autocomplete
+                      v-model="saleDto.itemtransactionDto.productDto"
+                      item-text="title"
+                      item-title="title"
+                      :items="productList"
+                      label="Product"
+                      name="title"
+                      return-object
+                      required
+                      density="compact"
+                      variant="outlined"
+                      filled
+                      class="cinput"
+                    />
+                  </v-col>
+
+                  <v-col cols="6" md="6">
+                    <v-text-field
+                      class="cinput"
+                      label="Qty"
+                      v-model="saleDto.itemtransactionDto.qty"
+                      variant="outlined"
+                      density="compact"
+                      type="number"
+                    />
+                  </v-col>
+
+                  <v-col cols="6" md="6">
+                    <v-text-field
+                      class="cinput"
+                      label="UnitPrice"
+                      v-model="saleDto.itemtransactionDto.unitPrice"
+                      variant="outlined"
+                      density="compact"
+                      type="number"
+                    />
+                  </v-col>
+                  <v-col cols="12" md="12">
+                    <v-text-field
+                      class="cinput"
+                      label="Amount"
+                      v-model="saleDto.itemtransactionDto.amount"
+                      variant="outlined"
+                      density="compact"
+                      type="number"
+                    />
+                  </v-col>
+                  <v-col cols="6" md="6">
+                    <v-text-field
+                      class="cinput"
+                      label="Discount"
+                      v-model="saleDto.itemtransactionDto.discount"
+                      variant="outlined"
+                      density="compact"
+                      type="number"
+                    />
+                  </v-col>
+                  <v-col cols="6" md="6">
+                    <v-text-field
+                      class="cinput"
+                      label="Balance"
+                      v-model="saleDto.itemtransactionDto.balance"
+                      variant="outlined"
+                      density="compact"
+                    />
+                  </v-col>
+
+                  <v-col cols="12" md="12" class="text-right">
+                    <v-btn class="bg-primary" @click="saveItem()">{{ saveOrUpdate }}</v-btn>
+                  </v-col>
+                </v-row>
+              </div>
+              <v-col cols="12" md="12" class="pt-2">
+                <v-text-field
+                  class="cinput"
+                  label="Total"
+                  v-model="totalBalance"
+                  variant="outlined"
+                  density="compact"
+                  readonly
+                />
+              </v-col>
+              <v-col cols="12" md="12" class="text-right">
+                <v-btn class="bg-primary" @click="saveSale()">{{ saveUpdate }}</v-btn>
+              </v-col>
+            </v-row>
+          </v-card-text>
+
+          <v-divider></v-divider>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
-import cityService from '../../service/CityService.js'
+import { format } from 'date-fns'
+import UserAccountService from '../../service/UserAccountService.js'
+import ProductService from '../../service/ProductService.js'
+import SaleService from '../../service/SaleService.js'
 export default {
   data() {
     return {
+      receivedPicker: new Date(),
+      fromMenu: false,
       dialog: false,
       cityName: '',
       townshipDto: {},
+      saleDto: {
+        itemtransactionDto: {
+          productDto: {},
+          qty: 0,
+          unitPrice: 0,
+          amount: 0,
+          discount: 0,
+          balance: 0,
+        },
+        transactionDto: {
+          balance: 0,
+        },
+      },
       selectedOne: {},
       saveOrUpdate: 'SAVE',
       dialogDelete: false,
-      townshipList: [
-        {
-          townshipId: 1,
-          townshipName: 'Yangon',
-        },
-        {
-          townshipId: 2,
-          townshipName: 'Mandalay',
-        },
-        {
-          townshipId: 3,
-          townshipName: 'Naypyidaw',
-        },
-        {
-          townshipId: 4,
-          townshipName: 'Naypyidaw',
-        },
-        {
-          townshipId: 5,
-          townshipName: 'Naypyidaw',
-        },
-        {
-          townshipId: 6,
-          townshipName: 'Naypyidaw',
-        },
-        {
-          townshipId: 7,
-          townshipName: 'Naypyidaw',
-        },
-        {
-          townshipId: 8,
-          townshipName: 'Naypyidaw',
-        },
-        {
-          townshipId: 9,
-          townshipName: 'Naypyidaw',
-        },
-        {
-          townshipId: 10,
-          townshipName: 'Naypyidaw',
-        },
-        {
-          townshipId: 11,
-          townshipName: 'Naypyidaw',
-        },
-        {
-          townshipId: 12,
-          townshipName: 'Naypyidaw',
-        },
-      ],
+      itemList: [],
+      customerList: [],
+      productList: [],
+      selectedIndex: null,
+      saveUpdate: 'SAVE',
     }
   },
   props: {},
-  mounted: function () {},
+  computed: {
+    totalBalance() {
+      return this.itemList.reduce((total, item) => {
+        return total + (Number(item.itemtransactionDto?.balance) || 0)
+      }, 0)
+    },
+  },
+  mounted: function () {
+    this.saleDto.receivedDate = format(this.receivedPicker, 'dd-MM-yyyy')
+    this.customerListMethod()
+    this.productListMethod()
+  },
   methods: {
-    cityListMethod() {
-      cityService
-        .getCity()
+    customerListMethod() {
+      UserAccountService.getUserAccount('CUSTOMER').then((response) => {
+        this.customerList.splice(0, this.customerList.length)
+        this.customerList.push(...response)
+        this.saleDto.customerDto = this.customerList[0]
+      })
+      // .catch((error) => {
+      //   this.$swal('Fail!', error.response.data.message, 'error')
+      // })
+    },
+    productListMethod() {
+      ProductService.getProduct().then((response) => {
+        this.productList.splice(0, this.productList.length)
+        this.productList.push(...response)
+        this.saleDto.itemtransactionDto.productDto = this.productList[0]
+      })
+      // .catch((error) => {
+      //   this.$swal('Fail!', error.response.data.message, 'error')
+      // })
+    },
+    calculateAmount() {
+      const qty = Number(this.saleDto.itemtransactionDto.qty) || 0
+      const unitPrice = Number(this.saleDto.itemtransactionDto.unitPrice) || 0
+      const discount = Number(this.saleDto.itemtransactionDto.discount) || 0
+      this.saleDto.itemtransactionDto.amount = qty * unitPrice
+      this.saleDto.itemtransactionDto.balance = this.saleDto.itemtransactionDto.amount - discount
+    },
+    saveItem() {
+      const item = JSON.parse(JSON.stringify(this.saleDto))
+
+      if (this.saveOrUpdate === 'SAVE') {
+        this.itemList.push(item)
+      } else {
+        const index = this.itemList.findIndex(
+          (x) =>
+            x.itemtransactionDto.productDto.productId ===
+            item.itemtransactionDto.productDto.productId,
+        )
+
+        if (index !== -1) {
+          this.itemList[index] = item
+        }
+
+        this.saveOrUpdate = 'SAVE'
+      }
+
+      this.clearItem()
+    },
+    editItem(item) {
+      this.saveOrUpdate = 'UPDATE'
+
+      this.saleDto = JSON.parse(JSON.stringify(item))
+    },
+    clearItem() {
+      this.saleDto.itemtransactionDto = {
+        productDto: this.productList[0] || {},
+        qty: 0,
+        unitPrice: 0,
+        amount: 0,
+        discount: 0,
+        balance: 0,
+      }
+    },
+    deleteItem(item) {
+      const index = this.itemList.indexOf(item)
+
+      if (index !== -1) {
+        this.itemList.splice(index, 1)
+      }
+    },
+
+    saveSale() {
+      const request = {
+        receivedDate: this.saleDto.receivedDate,
+        userAccount: this.saleDto.customerDto,
+
+        transaction: {
+          payment: this.totalBalance,
+        },
+
+        itemList: this.itemList.map((item) => {
+          return {
+            productId: item.itemtransactionDto.productDto.productId,
+            qty: item.itemtransactionDto.qty,
+            unitPrice: item.itemtransactionDto.unitPrice,
+            amount: item.itemtransactionDto.amount,
+            discount: item.itemtransactionDto.discount,
+            balance: item.itemtransactionDto.balance,
+          }
+        }),
+      }
+
+      SaleService.addNewSale(request)
         .then((response) => {
-          this.townshipList.splice(0, this.townshipList.length)
-          this.townshipList.push(...response)
+          // this.$swal('Success!', 'Sale saved successfully', 'success')
+
+          this.clearSale()
         })
         .catch((error) => {
           this.$swal('Fail!', error.response.data.message, 'error')
         })
     },
-    saveCity() {
-      if (this.saveOrUpdate == 'SAVE') {
-        console.log(this.saveOrUpdate)
 
-        cityService
-          .addCity(this.townshipDto)
-          .then((response) => {})
-          .catch((error) => {
-            // this.$swal('Fail!', error.response.data.message, 'error')
-          })
-      } else {
-        console.log(this.saveOrUpdate)
+    clearSale() {
+      this.itemList = []
 
-        cityService
-          .updateCity(this.townshipDto)
-          .then((response) => {})
-          .catch((error) => {
-            // this.$swal('Fail!', error.response.data.message, 'error')
-          })
+      this.saleDto.itemtransactionDto = {
+        productDto: this.productList[0] || {},
+        qty: 0,
+        unitPrice: 0,
+        amount: 0,
+        discount: 0,
+        balance: 0,
       }
-    },
-    editCity(item) {
-      console.log(item)
-      this.dialog = true
-      this.saveOrUpdate = 'UPDATE'
-      this.townshipDto = { ...item }
-    },
-    deletCity(item) {
-      this.dialogDelete = true
-      this.selectedOne = { ...item }
-      console.log(item)
-    },
-    clickDeleteDialog() {
-      cityService
-        .deleteCity(this.selectedOne)
-        .then((response) => {
-          this.dialogDelete = false
-        })
-        .catch((error) => {
-          // this.$swal('Fail!', error.response.data.message, 'error')
-        })
+
+      this.saveUpdate = 'SAVE'
     },
   },
-  watch: {},
+  watch: {
+    receivedPicker() {
+      this.fromMenu = false
+      this.saleDto.receivedDate = format(this.receivedPicker, 'dd-MM-yyyy')
+    },
+    'saleDto.itemtransactionDto.qty'() {
+      this.calculateAmount()
+    },
+
+    'saleDto.itemtransactionDto.unitPrice'() {
+      this.calculateAmount()
+    },
+    'saleDto.itemtransactionDto.discount'() {
+      this.calculateAmount()
+    },
+  },
   components: {},
 }
 </script>
@@ -294,14 +457,20 @@ td {
   color: white;
 }
 
+.cinput {
+  max-width: 100%;
+}
+
 .cinput :deep(.v-field) {
-  box-shadow: 0 0 0 3px rgba(35, 32, 33, 0.15);
+  box-shadow: 0 0 0 2px rgba(35, 32, 33, 0.15);
   padding-left: 12px;
+  min-height: 20px;
 }
 
 .cinput :deep(.v-field--focused) {
-  box-shadow: 0 0 0 3px rgba(35, 32, 33, 0.15);
+  box-shadow: 0 0 0 2px rgba(35, 32, 33, 0.15);
   padding-left: 12px;
+  min-height: 20px;
 }
 
 .cinput :deep(.v-label.v-field-label) {
